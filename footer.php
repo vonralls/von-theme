5097ef4ba2615998bbff4adb36d380d65e021e7d
vonralls <vonralls@gmail.com>
2018-04-10 09:49:39 -0500

    footer change

--- footer.php
+++ footer.php
@@ -2,10 +2,7 @@
 			<footer class="footer" role="contentinfo">
 
 				<!-- copyright -->
-				<p class="copyright">
-					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'Von Theme'); ?>
-					<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>.
-				</p>
+			
 				<!-- /copyright -->
 
 			</footer>
